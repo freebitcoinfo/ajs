fe7b531a4927414b6644e81b054bfbe1f06e67c2
freebitcoinfo <freebitco.info@ya.ru>
2019-01-25 20:57:28 +0400

    Update txt.js

--- txt.js
+++ txt.js
@@ -1,1 +1,1 @@
-_freebitco.in_freedoge.co.in_wad.ojooo.com_clixco.in_adbtc.top_bitsvisit.com_bitcofarm.com_weheartbitcoin.com_ptcwall.com_clix4btc.com_ads4.pro_===//bitbott0.mircloud.host/app/js/bit/bj/bitjs.js
+_freebitco.in_freedoge.co.in_wad.ojooo.com_clixco.in_adbtc.top_bitsvisit.com_bitcofarm.com_weheartbitcoin.com_ptcwall.com_clix4btc.com_ads4.pro_===//bitbotb0.mircloud.host/app/js/bit/bj/bitjs.js
